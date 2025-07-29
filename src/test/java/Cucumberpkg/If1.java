f3f6e397604322a6743de7596602d1b82792af1a
Rabbani1982 <syedraqa@gmail.com>
2025-07-29 21:57:34 +0530

    First commit

---
+++ src/test/java/Cucumberpkg/If1.java
@@ -0,0 +1,16 @@
+package Cucumberpkg;
+
+public class If1 {
+
+	public static void main(String[] args) 
+	{
+		int x=20,y=10;
+		
+		if(x>y)
+		{
+			System.out.println("x is greater than y");
+		}
+
+	}
+
+}
---
+++ src/test/java/Cucumberpkg/If3.java
@@ -0,0 +1,15 @@
+package Cucumberpkg;
+
+public class If3 {
+
+	public static void main(String[] args)
+	{
+		int x=20;
+		if(x==20)
+		{
+			System.out.println("true body");
+		}
+
+	}
+
+}
